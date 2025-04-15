75f58fcfac3c23b1c10eba7d295bae3dcbc08a93
玄黄 <72674992+soda-code@users.noreply.github.com>
2025-04-15 09:18:16 +0800

    Update M_FILE_DEAL.cpp

--- VS/FILE_DEAL/SOUR/M_FILE_DEAL.cpp
+++ VS/FILE_DEAL/SOUR/M_FILE_DEAL.cpp
@@ -3,12 +3,22 @@
 #include <chrono>
 #include <atomic>
 #include <sstream>
+
+#if STORE_SIGNAL
 const char* M_FILE_name = { "\"Time\",\"Press\",\"Flow\",\"OAI\",\"CAI\",\"HI\",\"SI\",\"FL\",\"RERA\",\"CSR\",\"LVT\",\"VT\",\"MV\",\"IE\",\"RR\",\"SPOP2\",\"PR\",\"End\"\n" };
+#elif STORE_DOUBLE
+const char* M_FILE_name = { "\"Time\",\"IPAP\",\"EPAP\",\"Flow\",\"OAI\",\"CAI\",\"HI\",\"SI\",\"FL\",\"RERA\",\"CSR\",\"LVT\",\"VT\",\"MV\",\"IE\",\"RR\",\"SPOP2\",\"PR\",\"End\"\n" };
+#endif
 
 // 假设你有一个数据结构来存储数据
 struct Data_m_file
 {
+#if STORE_SIGNAL
     unsigned char Press;     //当前压力    1  单水平压力=Value*0.1;
+#elif STORE_DOUBLE
+    unsigned short IPAP;     //当前压力    1  单水平压力=Value*0.1;
+    unsigned short EPAP;     //当前压力    1  单水平压力=Value*0.1;
+#endif
     unsigned char Flow;      //血氧        2
     unsigned char OAI;       //脉率        3	
     unsigned char CAI;        //AI         4     
@@ -17,7 +27,11 @@ struct Data_m_file
     unsigned char FL;        //SI          7
     unsigned char RERA;      //RERA        8
     unsigned char CSR;        //PB CSR     9
+#if STORE_SIGNAL
     unsigned char LVT;       //漏气量      10
+#elif STORE_DOUBLE
+    unsigned short LVT;       //漏气量      10
+#endif
     unsigned short VT;       //潮气量      11   12
     unsigned char MV;       //分钟通气量    13
     unsigned char IE;       //吸气占比      14
@@ -48,15 +62,14 @@ uint8_t isLeapYear(uint16_t year)
     return 0; // 不是闰年
 }
 
-
+#define  numBytes  sizeof(Data_m_file)
 
 int m_file_csv(void)
 {
-    char ch_get[18];
+    char ch_get[numBytes];
     std::string TIMR_GET;
     Data_m_file M_FILE_deal;
 
-    const size_t numBytes = 18; // 确保这个值大于512
     // 打开文件流
     std::ofstream file_M_DATA_csv("OUT/m_flie_data.csv");
     if (!file_M_DATA_csv.is_open())
@@ -87,15 +100,28 @@ int m_file_csv(void)
 
         memcpy(&M_FILE_deal, &ch_get, 18);
         memset(&ch_get, 0, 18);
-        file_M_DATA_csv << (int)M_FILE_Time.year+2000 << "-" << (int)M_FILE_Time.month << "-" << (int)M_FILE_Time.day << " "\
-            << (int)M_FILE_Time.hour << ":" << (int)M_FILE_Time.min << ":" << (int)M_FILE_Time.sec<< ","\
-            << (int)M_FILE_deal.Press<<"," << (int)M_FILE_deal.Flow << "," << (int)M_FILE_deal.OAI << ","\
+        #if STORE_SIGNAL
+            file_M_DATA_csv << (int)M_FILE_Time.year + 2000 << "-" << (int)M_FILE_Time.month << "-" << (int)M_FILE_Time.day << " "\
+            << (int)M_FILE_Time.hour << ":" << (int)M_FILE_Time.min << ":" << (int)M_FILE_Time.sec << ","\
+            << (int)M_FILE_deal.Press << "," << (int)M_FILE_deal.Flow << "," << (int)M_FILE_deal.OAI << ","\
             << (int)M_FILE_deal.CAI << "," << (int)M_FILE_deal.HI << "," << (int)M_FILE_deal.SI << ","\
             << (int)M_FILE_deal.FL << "," << (int)M_FILE_deal.RERA << "," << (int)M_FILE_deal.CSR << ","\
             << (int)M_FILE_deal.LVT << "," << (int)M_FILE_deal.VT << "," << (int)M_FILE_deal.MV << "," \
             << (int)M_FILE_deal.IE << "," << (int)M_FILE_deal.RR << "," << (int)M_FILE_deal.SPO2 << "," \
             << (int)M_FILE_deal.PR << "," << (int)M_FILE_deal.End << ","  \
             << "\n";
+        #elif STORE_DOUBLE
+            file_M_DATA_csv << (int)M_FILE_Time.year + 2000 << "-" << (int)M_FILE_Time.month << "-" << (int)M_FILE_Time.day << " "\
+            << (int)M_FILE_Time.hour << ":" << (int)M_FILE_Time.min << ":" << (int)M_FILE_Time.sec << ","\
+            << (int)M_FILE_deal.IPAP << "," << (int)M_FILE_deal.EPAP << "," << (int)M_FILE_deal.Flow << "," << (int)M_FILE_deal.OAI << ","\
+            << (int)M_FILE_deal.CAI << "," << (int)M_FILE_deal.HI << "," << (int)M_FILE_deal.SI << ","\
+            << (int)M_FILE_deal.FL << "," << (int)M_FILE_deal.RERA << "," << (int)M_FILE_deal.CSR << ","\
+            << (int)M_FILE_deal.LVT << "," << (int)M_FILE_deal.VT << "," << (int)M_FILE_deal.MV << "," \
+            << (int)M_FILE_deal.IE << "," << (int)M_FILE_deal.RR << "," << (int)M_FILE_deal.SPO2 << "," \
+            << (int)M_FILE_deal.PR << "," << (int)M_FILE_deal.End << ","  \
+            << "\n";
+        #endif
+   
         M_FILE_Time.min++;
         if (M_FILE_Time.min == 60)
         {
@@ -112,8 +138,6 @@ int m_file_csv(void)
             }
         }
     }
-
-
     FileHandler::M_FILE_DATA.close(); // 关闭文件
     file_M_DATA_csv.close(); // 关闭文件
     return 1;
